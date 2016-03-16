9c157364bbede769509007c2afe6b1d26689a0ef
Franck Legros <franck.legros@transatel.com>
2016-03-16 13:50:46 +0100

    http://www.jmdoudoux.fr/java/dej/chap-objets-mock.htm Done

--- src/test/java/chap102/testsauto/mock/monservice/MonServiceTest.java
+++ src/test/java/chap102/testsauto/mock/monservice/MonServiceTest.java
@@ -1,6 +1,7 @@
 package chap102.testsauto.mock.monservice;
 
 import org.easymock.EasyMock;
+import org.easymock.EasyMock.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,15 +29,39 @@ public class MonServiceTest {
     }
 
     @Test
+    public void testAdditionner(){
+
+        long retour =0l;
+
+        EasyMock.expect(mockCalulatrice.additionner(1,2)).andReturn(Long.valueOf(3l));
+        EasyMock.replay(mockCalulatrice);
+
+        retour = monService.additionner(1,2);
+        Assert.assertEquals("Valeur retournee est invalide",3l,retour);
+    }
+
+    @Test
     public void testAdditionnerParamreInvalide(){
 
         long retour =0l;
 
-        EasyMock.expect(mockCalulatrice.additionner(1,2)).andThrow(new IllegalArgumentException());
+        EasyMock.expect(mockCalulatrice.additionner(-1,2)).andThrow(new IllegalArgumentException());
         EasyMock.replay(mockCalulatrice);
 
         retour = monService.additionner(-1, 2);
         Assert.assertEquals("Valeur retournée esi invalide", -1l, retour);
     }
 
+    @Test
+    public void testCalculer(){
+        long retour=0l;
+
+        EasyMock.expect(mockCalulatrice.additionner(20,30)).andReturn(Long.valueOf(50l));
+        EasyMock.expect(mockCalulatrice.soustraire(50,10)).andReturn(Long.valueOf(40l));
+        EasyMock.replay(mockCalulatrice);
+
+        retour = monService.calculer(20,30,10);
+//        EasyMock.verify(mockCalulatrice);
+        Assert.assertEquals("Valeur retournée est invalide",40l,retour);
+    }
 }
